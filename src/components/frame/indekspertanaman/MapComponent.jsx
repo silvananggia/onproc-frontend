e96a14b17a2699b621d04b36493bc81a8ead51e5
silvananggia <silvananggia@gmail.com>
2025-10-27 12:05:35 +0700

    update

--- src/components/frame/indekspertanaman/MapComponent.jsx
+++ src/components/frame/indekspertanaman/MapComponent.jsx
@@ -2,7 +2,6 @@ import React, { useState, useRef } from "react";
 import Map from "./Map";
 import GeolocationButton from "./GeolocationButton";
 import BasemapSelect from "./BasemapSelect";
-import DrawButton from "../../map/DrawButton";
 
 
 const MapComponent = ({ bbox, selectedItem, collectionId, onAoiChange, isWorkspaceVisible, toggleWorkspace }) => {
@@ -16,11 +15,7 @@ const MapComponent = ({ bbox, selectedItem, collectionId, onAoiChange, isWorkspa
       <Map map={map} setMap={setMap} vectorLayerRef={vectorLayerRef} bbox={bbox} selectedItem={selectedItem} collectionId={collectionId} />
       {map && (
         <div className="button-container">
-          <DrawButton 
-            map={map} 
-            vectorLayerRef={vectorLayerRef} 
-            onAoiChange={onAoiChange}
-          />
+
           <GeolocationButton map={map} />
           <BasemapSelect map={map} />
         </div>
